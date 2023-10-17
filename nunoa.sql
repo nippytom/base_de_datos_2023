96788528b6e95bb9926900bd6f4052ae831acfb1
nippytom <128167950+nippytom@users.noreply.github.com>
2023-10-17 19:37:44 -0300

    query

--- nunoa.sql
+++ nunoa.sql
@@ -4,6 +4,7 @@ JUNTAS_VECINOS: id, numero, nombre, comunas_id
 LOCALES: id, rol, pago, direccion, giro, patentes_id, representantes_id, juntas_vecinos_id, comunas_id
 PATENTES: id, nombre, tipo*/
 
---｡･:*:･ﾟ★,｡･:*:･ﾟ☆QUERIES｡･:*:･ﾟ★,｡･:*:･ﾟ☆--
+--｡･:*:･ﾟ★,｡･:*:･ﾟ☆QUERIES･ﾟ☆｡･:*:･ﾟ★,｡･:*:｡･-- 
 
---1. 
+--1. Mostrar el nombre social del rut perteneciente a 97004000-5
+SELECT nombre_social FROM representantes WHERE rut = 97004000 AND dv = '5';
