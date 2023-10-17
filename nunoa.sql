f6bd07a32267c7356b3c492c1b35083b65031cb7
nippytom <128167950+nippytom@users.noreply.github.com>
2023-10-17 19:40:46 -0300

    query

--- nunoa.sql
+++ nunoa.sql
@@ -8,3 +8,35 @@ PATENTES: id, nombre, tipo*/
 
 --1. Mostrar el nombre social del rut perteneciente a 97004000-5
 SELECT nombre_social FROM representantes WHERE rut = 97004000 AND dv = '5';
+
+--2. Mostrar las direcciones de los locales que corresponden al rut 97004000-5
+
+
+--3. ¿Cuantos locales hay en comuna de Ñuñoa?
+
+
+--4. ¿Cuantos locales tiene el rut 97023000-9 en la junta de vecinos numero 7?
+
+
+--5. ¿Cuantos locales hay en la unidad vecinal 18?
+
+
+--6. Mostrar la direccion con locales
+
+
+--7. Mostrar el nombre social de los representantes que tengan más de 3 locales y menos de 7 locales
+
+
+--8. Mostrar cuanto paga nombre social CENCOSUD RETAIL S.A. por la totalidad de sus patentes
+
+
+--9. Mostrar ordenadamente de mayor a menor quienes tienen mas locales en la comuna
+
+
+--10. Mostrar quien tiene más locales en la comuna
+
+
+--11. Mostrar los representantes que no estan presente en la junta de vecinos mayores al numero 30 y menores al numero 10
+
+
+--12. Mostrar el nombre social de quien más paga en las junta vecinos 4 y 16
