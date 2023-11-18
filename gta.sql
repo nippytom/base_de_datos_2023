4d2cd899acf3981a75d02d62ddf92af36020e183
nippytom <128167950+nippytom@users.noreply.github.com>
2023-11-17 23:49:00 -0300

    sql

--- gta.sql
+++ gta.sql
@@ -1,5 +1,5 @@
 /*
-Usuarios: id,nombre,nivel, psn_user
+Usuarios: id, nombre, nivel, psn_user
 UsuariosArmas: usuario_id, arma_id, municion
 Armas: id, modelo, tipo, maximo
 Misiones: id, nombre, nivel_minimo, dinero, experiencia
@@ -9,7 +9,7 @@ UsuariosMisiones: usuarios_id, mision_id, terminado
 --｡･:*:･ﾟ★,｡･:*:･ﾟ☆QUERIES･ﾟ☆｡･:*:･ﾟ★,｡･:*:｡･-- 
 
 --1. Nombre de usuarios que tienen el arma "Lanza Cohetes Teledirigido" con su municion al maximo
-SELECT psn_user FROM usuarios JOIN UsuarioArmas ON UsuariosArmas.usuario_id = usuario.id JOIN armas ON armas.id = UsuariosArmas.arma_id WHERE armas.modelo = "Lanza Cohetes Teledirigido" AND armas.maximo = "TRUE";
+
 
 --2. Nombre de usuarios de que han terminado exactamente 5 misiones
 SELECT psn_user FROM usuarios JOIN (SELECT usuario_id, COUNT(*) FROM UsuariosMisiones WHERE terminado = TRUE GROUP BY usuario_id) AS t1 ON t1.usuario_id = usuarios.id WHERE t1.COUNT = 5;
@@ -20,7 +20,8 @@ SELECT psn_user FROM usuarios JOIN (SELECT usuario_id, COUNT(*) FROM usuariosarm
 ...
 
 --4. Nombre del personaje que la suma de dinero que ha ganado sea mas de 1.000.000 dolares
-SELECT nombre FROM usuarios(SELECT usuario_id, SUM(dinero) FROM usuariosmisiones JOIN misiones ON misiones.id = usuariosmisiones.mision_id WHERE terminado = TRUE GROUP BY usuario_id) AS t1
+SELECT nombre FROM usuarios(SELECT usuario_id, SUM(dinero) FROM usuariosmisiones 
+JOIN misiones ON misiones.id = usuariosmisiones.mision_id WHERE terminado = TRUE GROUP BY usuario_id) AS t1
 ON t1.usuario_id = usuarios.idWHERE t1.dinero = 1000000;
 
 --5. Cuales son los usuarios que han realizado mas misiones distintas
@@ -31,3 +32,6 @@ SELECT psn_user FROM (SELECT MAX(COUNT) FROM (SELECT usuario_id, COUNT(*) FROM
 --6. Cuales son los usuarios con mas armas.
 SELECT psn_user FROM (SELECT MAX(COUNT) FROM (SELECT usuario_id, COUNT(*) FROM usuariosarmas GROUP BY usuario_id )as t1) as t2 
 JOIN (SELECT usuario_id, COUNT(*) FROM usuariosarmas GROUP BY usuario_id )as t3 ON t3.COUNT = t2.MAX JOIN usuarios ON usuarios.id = t3.usuario_id;
+
+--7. Cuales son los usuarios que tienen entre 10 y 20 armas.
+SELECT psn_user 
